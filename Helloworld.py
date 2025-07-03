c48229f60f5b3120b425a5fc921925b395a3cdb0
MANJIT <manjitchakraborty9@gmail.com>
2025-07-03 06:15:15 +0530

    Variable

---
+++ Helloworld.py
@@ -0,0 +1,20 @@
+print("Hello WOrld")
+name="manjit"
+age=15
+height = 5.6
+print(name)
+print(age)
+print(height)
+print(name , age)
+print(name, height)
+print("My name is ", name)
+Name = name
+print(Name)
+print(type(Name))
+print(type(age))
+print(type(height))
+old=True
+print(old)
+a=None
+print(a)
+print(type(a))
